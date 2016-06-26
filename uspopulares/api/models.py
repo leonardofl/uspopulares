a043713466120e833444d9c9f08cbc0fedd9d24d
Leonardo Leite <leonardofl87@gmail.com>
2016-06-26 18:03:24 -0300

    criando modelo Publicacao

--- uspopulares/api/models.py
+++ uspopulares/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Publicacao(models.Model):
+
+    DISSERTACAO = 'DISSERTACAO'
+    TESE = 'TESE'
+    LIVRE_DOCENCIA = 'LIVRE_DOCENCIA'
+    TIPOS = ((DISSERTACAO, 'Dissertação'), (TESE, 'Tese'), (LIVRE_DOCENCIA, 'Livre docência'))
+
+    titulo = models.TextField()
+    tipo = models.TextField(choices=TIPOS)
+    autor = models.TextField()
+    unidade = models.TextField()
+    area_conhecimento = models.TextField()
+    data_defesa = models.DateField()
+    ano_publicacao = models.IntegerField()
+    orientador = models.TextField()
+    palavras_chaves = models.TextField()
+
