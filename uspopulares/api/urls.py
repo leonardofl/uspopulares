ca50ca590fadc5c2ae46d3740e89fd83f679997b
Leonardo Leite <leonardofl87@gmail.com>
2016-07-02 19:45:06 -0300

    criando algumas views

--- uspopulares/api/urls.py
+++ uspopulares/api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^publicacoes$', views.publicacoes),
+    url(r'^publicacoes/$', views.publicacoes),
+    url(r'^publicacoes/(?P<publicacao_id>[0-9]+)/$', views.publicacao),
 ]
--- uspopulares/api/views.py
+++ uspopulares/api/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from api import models
 
 def publicacoes(request):
-    return HttpResponse("Retornando dados de todas as publicações.")
+    publicacoes = models.Publicacao.objects.all()
+    titulos = [ p.titulo for p in publicacoes ]
+    resposta = '</br>'.join(titulos)
+    return HttpResponse(resposta)
+
+def publicacao(request, publicacao_id):
+    publicacao = get_object_or_404(models.Publicacao, id=publicacao_id)
+    resposta = '%s</br>%s' % (publicacao.autor, publicacao.titulo)
+    return HttpResponse(resposta)
 
