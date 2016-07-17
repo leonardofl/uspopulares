64a7fab979dab6619033b5def780dc92d21af2db
Leonardo Leite <leonardofl87@gmail.com>
2016-07-17 15:57:38 -0300

    scrape de uma publicação completo

--- teses_usp_crawler/teses_usp_crawler/spiders/teses_spyder.py
+++ teses_usp_crawler/teses_usp_crawler/spiders/teses_spyder.py
@@ -10,18 +10,24 @@ class TesesSpider(scrapy.Spider):
 
     def parse(self, response):
         publicacao = items.PublicacaoItem()
-        publicacao['titulo'] = response.xpath('//input[@name=\'evTitulo\']/@value').extract()
-        publicacao['tipo'] = response.xpath('//div[contains(text(), \'Documento\')]/following-sibling::div[1]/a/text()').extract()
-        publicacao['autor'] = response.xpath('//div[contains(text(), \'Nome completo\')]/following-sibling::div[1]/text()').extract()
-        publicacao['unidade'] = response.xpath('//div[contains(text(), \'Unidade da USP\')]/following-sibling::div[1]/a/text()').extract()
-        #publicacao['area_conhecimento'] = 
-        publicacao['data_defesa'] = response.xpath('//div[contains(text(), \'Data de Defesa\')]/following-sibling::div[1]/a/text()').extract()
-        publicacao['ano_publicacao'] = response.xpath('//div[contains(text(), \'Imprenta\')]/following-sibling::div[1]/text()').re('\d+')
-        # publicacao['orientador'] =
-        # publicacao['banca'] =
-        # publicacao['palavras_chaves'] =
-        publicacao['visitas'] = response.xpath('//img[@alt=\'Visitas\']/parent::a/parent::div/following-sibling::div[1]/text()').extract()
-        publicacao['downloads'] = response.xpath('//img[@alt=\'Downloads\']/parent::a/parent::div/following-sibling::div[1]/text()').extract()
+        publicacao['titulo'] = response.xpath('//input[@name=\'evTitulo\']/@value').extract()[0]
+        publicacao['tipo'] = response.xpath('//div[contains(text(), \'Documento\')]/following-sibling::div[1]/a/text()').extract()[0]
+        publicacao['autor'] = response.xpath('//div[contains(text(), \'Nome completo\')]/following-sibling::div[1]/text()').extract()[0]
+        publicacao['unidade'] = response.xpath('//div[contains(text(), \'Unidade da USP\')]/following-sibling::div[1]/a/text()').extract()[0]
+        publicacao['area_conhecimento'] = response.xpath('//div[contains(text(), \'Área do Conhecimento\')]/following-sibling::div[1]/a/text()').extract()[0]
+        publicacao['data_defesa'] = response.xpath('//div[contains(text(), \'Data de Defesa\')]/following-sibling::div[1]/a/text()').extract()[0]
+        publicacao['ano_publicacao'] = response.xpath('//div[contains(text(), \'Imprenta\')]/following-sibling::div[1]/text()').re('\d+')[0]
+        publicacao['orientador'] = response.xpath('//meta[@name=\'DC.contributor\']/@content').extract()[0]
+        banca = response.xpath('//div[contains(text(), \'Banca examinadora\')]/following-sibling::div[1]/text()').extract()
+        banca = [b.replace('\n', '') for b in banca]
+        banca.remove('')
+        publicacao['banca'] = banca
+        palavras_chaves = response.xpath('//div[contains(text(), \'Palavras-chave em português\')]/following-sibling::div[1]/text()').extract()
+        palavras_chaves = [p.replace('\n', '') for p in palavras_chaves]
+        palavras_chaves.remove('')
+        publicacao['palavras_chaves'] = palavras_chaves
+        publicacao['visitas'] = response.xpath('//img[@alt=\'Visitas\']/parent::a/parent::div/following-sibling::div[1]/text()').extract()[0]
+        publicacao['downloads'] = response.xpath('//img[@alt=\'Downloads\']/parent::a/parent::div/following-sibling::div[1]/text()').extract()[0]
         publicacao['url'] = response.url
         yield publicacao
 
